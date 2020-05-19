1f10f9024c35070c31b52c2bc2b92ebfee67127a
Gian Antonio Batista <gian.antoniobatista@gmail.com>
2020-05-19 17:15:07 -0300

    Criando Projeto e testando a classe CategoriaResouce

---
+++ src/main/java/com/gianprog/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,15 @@
+package com.gianprog.cursomc.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+	
+	@GetMapping
+	public String mostrar() {
+		return "REST está funcionando";
+	}
+}
