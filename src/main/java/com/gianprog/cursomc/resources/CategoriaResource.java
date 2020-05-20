f2b0a36c92cce2542a367c7661036fcaad742bfa
Gian Antonio Batista <gian.antoniobatista@gmail.com>
2020-05-20 13:13:47 -0300

    Testando a primeira classe de domínio

--- src/main/java/com/gianprog/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/gianprog/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.gianprog.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gianprog.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String mostrar() {
-		return "REST está funcionando";
+	public List<Categoria> mostrar() {
+		Categoria cat1 = new Categoria(1, "Gian");
+		Categoria cat2 = new Categoria(2, "Giliard");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		
+		return lista;
 	}
 }
