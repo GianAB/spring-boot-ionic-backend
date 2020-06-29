4d3a457cb5c2f4001bf796fc0b2db54e786241f7
Gian Antonio Batista <gian.antoniobatista@gmail.com>
2020-06-29 17:05:21 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/gianprog/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/gianprog/cursomc/resources/CategoriaResource.java
@@ -1,13 +1,17 @@
 package com.gianprog.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gianprog.cursomc.domain.Categoria;
 import com.gianprog.cursomc.services.CategoriaService;
@@ -26,8 +30,20 @@ public class CategoriaResource {
 	}
 	
 	@GetMapping
-	public List<Categoria> findAll(){
-		List<Categoria> lista = service.findAll();
+	public List<?> findAll(){
+		List<?> lista = service.findAll();
 		return lista;
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(obj.getId())
+				.toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/gianprog/cursomc/services/CategoriaService.java
+++ src/main/java/com/gianprog/cursomc/services/CategoriaService.java
@@ -27,4 +27,9 @@ public class CategoriaService {
 		 return obj.orElseThrow(()-> new ObjectNotFoundException(
 				 "Objeto não encontrado! Id: " + id + ", tipo: " + Categoria.class.getName()));
 	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
 }
