20229d9e8accf8dd68742f90390ef90601e2a2e0
Gian Antonio Batista <gian.antoniobatista@gmail.com>
2020-07-05 16:55:10 -0300

    Paginação com parâmetros opcionais

--- src/main/java/com/gianprog/cursomc/CursomcApplication.java
+++ src/main/java/com/gianprog/cursomc/CursomcApplication.java
@@ -71,6 +71,11 @@ public class CursomcApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "informática");
 		Categoria cat2 = new Categoria(null, "escritório");
+		Categoria cat3 = new Categoria(null, "mesa e banho");
+		Categoria cat4 = new Categoria(null, "esporte");
+		Categoria cat5 = new Categoria(null, "moda");
+		Categoria cat6 = new Categoria(null, "lazer");
+		Categoria cat7 = new Categoria(null, "Decoração");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.0);
 		Produto p2 = new Produto(null, "Impressora", 800.0);
@@ -83,7 +88,7 @@ public class CursomcApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().add(cat1);
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
--- src/main/java/com/gianprog/cursomc/services/CategoriaService.java
+++ src/main/java/com/gianprog/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.gianprog.cursomc.domain.Categoria;
@@ -14,20 +17,20 @@ import com.gianprog.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public List<Categoria> findAll() {
 		return repository.findAll();
 	}
-	
-	public Categoria findById(Integer id) throws ObjectNotFoundException{
+
+	public Categoria findById(Integer id) throws ObjectNotFoundException {
 		Optional<Categoria> obj = repository.findById(id);
-		 return obj.orElseThrow(()-> new ObjectNotFoundException(
-				 "Objeto não encontrado! Id: " + id + ", tipo: " + Categoria.class.getName()));
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
@@ -41,9 +44,15 @@ public class CategoriaService {
 	public void deleteById(Integer id) {
 		findById(id);
 		try {
-		repository.deleteById(id);
-		}catch(DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não pode excluir uma categoria que esteja associada à um ou mais produtos!");
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException(
+					"Não pode excluir uma categoria que esteja associada à um ou mais produtos!");
 		}
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String direction, String orderBy) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
 	}
 }
