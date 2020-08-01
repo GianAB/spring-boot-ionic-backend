35d390b68b9032e9d7d2de5ab4387a3deb68fc68
Gian Antonio Batista <gian.antoniobatista@gmail.com>
2020-08-01 15:36:07 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/gianprog/cursomc/resources/ProdutoResource.java
+++ src/main/java/com/gianprog/cursomc/resources/ProdutoResource.java
@@ -3,13 +3,17 @@ package com.gianprog.cursomc.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gianprog.cursomc.domain.Produto;
+import com.gianprog.cursomc.dto.ProdutoDTO;
+import com.gianprog.cursomc.resources.utils.URL;
 import com.gianprog.cursomc.services.ProdutoService;
 
 @RestController
@@ -19,15 +23,27 @@ public class ProdutoResource {
 	@Autowired
 	private ProdutoService service;
 	
-	
-	@GetMapping
-	public ResponseEntity<List<Produto> >findAll() {
-		List<Produto> lista = service.findAll();
-		return ResponseEntity.ok().body(lista);
-	}
-	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Produto> findById(@PathVariable Integer id){
 		return ResponseEntity.ok().body(service.findById(id));
 	}
+	
+	@GetMapping
+	public Page<ProdutoDTO> findPage(
+			@RequestParam(value = "nome", defaultValue = "") String nome,
+			@RequestParam(value = "categorias", defaultValue = "0") String categorias,
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage",  defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "id") String orderBy
+			){
+		String nomeDecoded = URL.decodeParam(nome);
+		List<Integer> ids = URL.decodeIntList(categorias);
+		
+		Page<Produto> list = service.search(nomeDecoded, ids, page, linesPerPage, direction, orderBy);
+		Page<ProdutoDTO> listDto = list.map(x -> new ProdutoDTO(x));
+		
+		return listDto;
+	}
+	
 }
--- src/main/java/com/gianprog/cursomc/services/ProdutoService.java
+++ src/main/java/com/gianprog/cursomc/services/ProdutoService.java
@@ -4,9 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.gianprog.cursomc.domain.Categoria;
 import com.gianprog.cursomc.domain.Produto;
+import com.gianprog.cursomc.repositories.CategoriaRepository;
 import com.gianprog.cursomc.repositories.ProdutoRepository;
 import com.gianprog.cursomc.services.exception.ObjectNotFoundException;
 
@@ -15,10 +20,9 @@ public class ProdutoService {
 	
 	@Autowired
 	private ProdutoRepository repository;
-	
-	public List<Produto> findAll() {
-		return repository.findAll();
-	}
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	
 	public Produto findById(Integer id) {
 		Optional<Produto> obj = repository.findById(id);
@@ -26,5 +30,11 @@ public class ProdutoService {
 				"Objeto não encontrado! Id: " + id + ", Classe: " + Produto.class.getName()));
 	}
 	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String direction, String orderBy) {
+		PageRequest request = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		
+		return repository.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, request);
+	}
 	
 }
