f08366db53ad2cd39013698f751f393b9200eb9b
Gian Antonio Batista <gian.antoniobatista@gmail.com>
2020-08-12 13:23:11 -0300

    Criando Profile de desenvolvimento

---
+++ src/main/java/com/gianprog/cursomc/config/DevConfig.java
@@ -0,0 +1,31 @@
+package com.gianprog.cursomc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.gianprog.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instanciateDatabase() {
+		
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.instanciateTestBadabase();
+		return true;
+	}
+}
