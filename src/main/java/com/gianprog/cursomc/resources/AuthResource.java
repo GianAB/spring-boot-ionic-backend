d2b5d276d53191e144753300c3a93c62cef0f0ec
Gian Antonio Batista <gian.antoniobatista@gmail.com>
2020-10-16 16:50:39 -0300

    Refresh token

---
+++ src/main/java/com/gianprog/cursomc/resources/AuthResource.java
@@ -0,0 +1,30 @@
+package com.gianprog.cursomc.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gianprog.cursomc.security.JWTUtil;
+import com.gianprog.cursomc.security.UserSpringSecurity;
+import com.gianprog.cursomc.services.UserServices;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@PostMapping(value = "/refresh_token")
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSpringSecurity user = UserServices.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
+		return ResponseEntity.noContent().build();
+	}
+}
